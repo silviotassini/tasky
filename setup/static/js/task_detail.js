b10e7d75f4c1d7fbc3d9fcea1d5392368dafca85
silviotassini <silviotassini@gmail.com>
2025-03-27 12:45:29 -0300

    update styles v.1

---
+++ setup/static/js/task_detail.js
@@ -0,0 +1,8 @@
+// Funções para adicionar nova tarefa
+function showTaskForm(cardId) {
+    document.getElementById('task-modal').style.display = 'block'
+}
+
+function closeTaskForm() {
+    document.getElementById('task-modal').style.display = 'none'
+}
